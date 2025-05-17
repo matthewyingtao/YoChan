a0afddd6ffbe0b3c9d4879e6362cdd22eb69c09c
Matthew Tao <matthew.yingtao@gmail.com>
2025-05-17 12:18:09 +1200

    Refactor file upload handling in app.ts

--- src/app.ts
+++ src/app.ts
@@ -4,10 +4,10 @@ import path from "path";
 import sharp from "sharp";
 
 const app = express();
-const uploadHandler = multer({  });
+const uploadHandler = multer({});
 
 app.get("/", (req, res) => {
-    res.send(`
+	res.send(`
         <form action="/" method="post" enctype="multipart/form-data">
             <input type="file" name="file" id="file">
             <input type="submit" value="Upload" name="submit">
@@ -15,31 +15,34 @@ app.get("/", (req, res) => {
     `);
 });
 
-app.post("/", uploadHandler.single('file'), (req, res) => {
-    const file = req.file;
+app.post("/", uploadHandler.single("file"), async (req, res) => {
+	const file = req.file;
 
-    if (!file) {
-        res.status(400).send("No file uploaded.");
-        return;
-    }
+	if (!file) {
+		res.status(400).send("No file uploaded.");
+		return;
+	}
 
-    const image = sharp(file.buffer).grayscale().toFile(`uploads/${file.originalname}`)
+	const image = await sharp(file.buffer)
+		.grayscale()
+		.toFile(path.join(path.dirname(__dirname), "uploads", file.originalname));
 
-    res.send(`
-        <p>File Path: ${file.originalname}</p>
+	res.send(`
+        <p>File Path: <a href="${new URL(
+					`/uploads/${file.originalname}`,
+					req.protocol + "://" + req.get("host")
+				)}">${file.originalname}</a></p>
     `);
-})
-
-app.get("/uploads/:filename", (req, res) => {
-    const filename = req.params.filename;
-    const filePath = `uploads/${filename}`;
-
-    console.log(`File Path: ${filePath}`);
-
-
-    res.sendFile(filePath, { root: path.dirname(__dirname) });
 });
 
+app.get("/uploads/:filename", (req, res) => {
+	const filename = req.params.filename;
+	const filePath = `uploads/${filename}`;
+
+	console.log(`File Path: ${filePath}`);
+
+	res.sendFile(filePath, { root: path.dirname(__dirname) });
+});
 
 app.listen(3000, () => {
 	console.log(`Server is running on port 3000`);
